19d30aadf927c598497c5c5bdd7676f4003151b9
Nicolas Lemoyne <nicolas@quatrecentquatre.com>
2021-08-16 15:09:24 -0400

    Build assets

--- dist/me-skin.js
+++ dist/me-skin.js
@@ -665,6 +665,7 @@ var SkinSelect = /*#__PURE__*/function (_SkinField3) {
       }
 
       var selects = $('select:not(' + this.$field.attr('name') + ')');
+      console.log(selects);
       selects.each(function (index, value) {
         Me.skin.getField($(value)).close();
       });
