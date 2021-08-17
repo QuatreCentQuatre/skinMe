157ed3788f492939c180c913b959be1742933c43
Nicolas Lemoyne <nicolas@quatrecentquatre.com>
2021-08-16 21:53:48 -0400

    combined

--- dist/me-skin.js
+++ dist/me-skin.js
@@ -665,11 +665,12 @@ var SkinSelect = /*#__PURE__*/function (_SkinField3) {
       }
 
       var selects = $('select:not(' + this.$field.attr('name') + ')');
-      console.log(selects);
       selects.each(function (index, value) {
-        console.log('0.1', index, value, Me.skin.getField, Me.skin.fields);
-        console.log('1', Me.skin.getField($(value)), $(value));
-        Me.skin.getField($(value)).close();
+        var field = Me.skin.getField($(value));
+
+        if (field) {
+          field.close();
+        }
       });
       this.isAnimating = true;
       this.$skinChoicesWrapper.outerHeight(this.choicesHeight);
