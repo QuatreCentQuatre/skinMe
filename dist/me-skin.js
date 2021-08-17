3e18ff0858ba7e6eb96332be53aef9e5caa9f9b7
Nicolas Lemoyne <nicolas@quatrecentquatre.com>
2021-08-16 21:06:13 -0400

    Combined with logs on selects each for debug purpose

--- dist/me-skin.js
+++ dist/me-skin.js
@@ -665,10 +665,10 @@ var SkinSelect = /*#__PURE__*/function (_SkinField3) {
       }
 
       var selects = $('select:not(' + this.$field.attr('name') + ')');
-      console.log(selects); // selects.each(function (index, value) {
-      // 	Me.skin.getField($(value)).close();
-      // });
-
+      console.log(selects);
+      selects.each(function (index, value) {
+        console.log(index, value, Me.skin.getField); // 	Me.skin.getField($(value)).close();
+      });
       this.isAnimating = true;
       this.$skinChoicesWrapper.outerHeight(this.choicesHeight);
       this.$wrapper.addClass(this.classes.opening).on('transitionend', function () {
